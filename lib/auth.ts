2456f71446b5e2faa5d2abb23b431de160bc9767
joshuavilela <joshuavilela1@gmail.com>
2023-03-01 17:00:41 -0800

    Added AuthForm Component to register/sigin pages

---
+++ lib/auth.ts
@@ -0,0 +1,7 @@
+import bcrypt from 'bcrypt';
+
+export const hashPassword = () => bcrypt.hash(password, 10);
+
+export const comparePasswords = (plainTextPassword, hashedPassword) => {
+  bcrypt.compare(plainTextPassword, hashedPassword);
+};
