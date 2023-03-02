9af5287cb5be398e1cea4159edb897a7c43f7739
joshuavilela <joshuavilela1@gmail.com>
2023-03-01 17:43:55 -0800

    Added API Routes for Sign in and Register Pages

--- lib/auth.ts
+++ lib/auth.ts
@@ -2,7 +2,7 @@ import bcrypt from 'bcrypt';
 import { SignJWT, jwtVerify } from 'jose';
 import { db } from './db';
 
-export const hashPassword = () => bcrypt.hash(password, 10);
+export const hashPassword = (password: string) => bcrypt.hash(password, 10);
 
 export const comparePasswords = (plainTextPassword, hashedPassword) => {
   bcrypt.compare(plainTextPassword, hashedPassword);
